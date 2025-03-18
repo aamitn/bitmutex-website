e51352e123ba3e90ec3c6dbd9261dea5022e48dc
Amit Nandi <amitnandileo@gmail.com>
2025-03-18 18:30:03 +0530

    fixed footer issues

--- client/src/components/custom/status-badge.tsx
+++ client/src/components/custom/status-badge.tsx
@@ -19,37 +19,34 @@ export default function StatusBadge() {
     }, [theme, mounted]);
 
     return (
-<div
-  className={`transition-opacity duration-500 ease-in-out opacity-0 animate-fade-in 
+        <div
+            className={`transition-opacity duration-500 ease-in-out opacity-0 animate-fade-in 
               bg-opacity-10 rounded-lg p-1 flex md:inline-flex items-center justify-center 
               text-center w-full md:w-auto`}
-    style={{
-    background: mounted
-        ? theme === "dark"
-            ? "rgba(255,255,255,0.1)"
-            : "rgba(0,0,0,0.05)"
-        : "transparent", // Prevent hydration mismatch
-    borderRadius: "6px",
-    padding: "4px",
-    opacity: mounted ? 1 : 0,
-    transition: "opacity 0.5s ease-in-out",
-    }}
->
-<div className="flex flex-col sm:flex-row items-center ml-20 md:ml-0">
-  {mounted && iframeSrc && (
-                <iframe
-                    src={iframeSrc}
-                    width="250"
-                    height="30"
-                    frameBorder="0"
-                    scrolling="no"
-                    style={{ colorScheme: "normal" }}
-                ></iframe>
-            )}
-  </div>
-</div>
-
-
-
+            style={{
+                background: mounted
+                    ? theme === "dark"
+                        ? "rgba(255,255,255,0.1)"
+                        : "rgba(0,0,0,0.05)"
+                    : "transparent", // Prevent hydration mismatch
+                borderRadius: "6px",
+                padding: "4px",
+                opacity: mounted ? 1 : 0,
+                transition: "opacity 0.5s ease-in-out",
+            }}
+        >
+            <div className="flex flex-col sm:flex-row items-center ml-20 md:ml-0">
+                {mounted && iframeSrc && (
+                    <iframe
+                        src={iframeSrc}
+                        width="250"
+                        height="30"
+                        frameBorder="0"
+                        scrolling="no"
+                        style={{ colorScheme: "normal" }}
+                    ></iframe>
+                )}
+            </div>
+        </div>
     );
 }
